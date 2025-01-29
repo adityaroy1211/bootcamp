9405e7e22f6171e94dc4f2d639542e1639393b40
Aditya Roy <adityaroy1211@gmail.com>
2025-01-29 10:13:26 +0530

    changed

---
+++ day1/squarepattern.c
@@ -0,0 +1,67 @@
+// square pattern
+// #include<stdio.h>
+// int main()
+// {
+//     for (int i=1;i<=4;i++)
+//     {
+//         for (int j=1;j<=4;j++)
+//         {
+//             printf("%d",j);
+
+//         }
+//         printf("\n");
+    
+        
+    
+//     }
+    
+// }
+
+
+//right angled triangle
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i=1;i<=4;i++)
+//     {
+//         for(int j=1;j<=i;j++)
+//         {
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+    
+// } 
+
+//inverted right angled triangled
+// #include<stdio.h>
+// int main()
+// {
+//     for(int i=1;i<=4;i++)
+//     {
+//         for(int j=4;j>=i;j--)
+//         {
+//             printf("*");
+//         }d
+//         printf("\n");
+//     }
+    
+// }
+
+
+// floyd's triangle
+// #include<stdio.h>>
+// int main()
+// {
+//      int count=1;
+//     for(int i=0;i<4;i++)
+//     {
+//         for(int j=0;j<i+1;j++)
+//         {
+//             printf("%d",count);
+//             count++;
+//         }
+//         printf("\n");
+//     }
+// }
+
---
+++ day2/function.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+int main()
+
+// number is positive or negative
+
+
+// {
+//     int a;
+//     printf("enter aany number");
+//     scanf("%d",&a);
+//     if(a==0){
+//     printf("the number is zero");
+//     }
+//     else if(a>0){
+//         printf("the number is positive");
+//     }
+//     else
+//     printf("the number is negative");
+ 
+//     return 0;
+
+// }
+
+// --------------------------------------------------------------------------------------------------------
+
+// greatest between three numbers 
+// {
+//     int a,b,c;
+//     printf("enter any three numbers");
+//     scanf("%d%d%d",&a,&b,&c);
+//     if(a>b && a>c){
+//         printf("a is greatest");
+//     }
+//     else if(c>a)
+//     {
+//         printf("c is greatest");
+//     }
+//     else
+//     {
+//         printf("b is greatest");
+//     }
+//     return 0;
+
+// }
+// ------------------------------------------------------------------------------------------------------
+// vowel or consonant
+
+// {
+//     char ch;
+//     printf("enter a character:");
+//     scanf("%c",&ch);
+//     if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' || ch=='a' || ch=='e' || ch=='i' ||ch=='o' || ch=='u' )
+//     {
+//         printf("character is vowel");
+//     }
+//     else 
+//     {
+//         printf("character is consonent");
+//     }
+//     return 0;
+// }
+
+// -------------------------------------------------------------------------------------------------------
+
